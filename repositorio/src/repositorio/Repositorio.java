3084be0a14df1c9a094157dcb556c83b31036863
SalvadorProgrammer <ascolong@gmail.com>
2019-03-18 13:02:41 -0600

    miPrimerCommit2

--- repositorio/src/repositorio/Repositorio.java
+++ repositorio/src/repositorio/Repositorio.java
@@ -2,8 +2,17 @@ package repositorio;
 
 public class Repositorio {
 
+    String nombre;
+   int valor;    
+    
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    
     
 }
